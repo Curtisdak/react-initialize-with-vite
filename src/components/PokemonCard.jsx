e76f7f8b4d8493d5f47e4f45a2834b6221837f98
Curtisdak <curtis.dakouri@gmail.com>
2024-03-27 14:33:04 +0100

    This is fihnaly done

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,13 +1,43 @@
 
+import './PokemonCard.css'
+
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+      imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/19.png",
+        
+    },
+
+    {
+        name: "kirikou",
+       
+    },
+  ];
 
 function PokemonCard(){
-    return(
-       
-        <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-            <figcaption>Bulbasaur</figcaption>
-        </figure>
-    )
+    const pokemon =pokemonList[1]
+       if (!pokemon.imgSrc) {
+         return (
+           <>
+             <p>???</p>
+             <figcaption>{pokemon.name}</figcaption>
+           </>
+         );
+       } else {
+         return (
+           <figure>
+             <img src={pokemon.imgSrc} />
+             <figcaption>{pokemon.name}</figcaption>
+           </figure>
+         );
+       }
+
 }
 
 export default PokemonCard
