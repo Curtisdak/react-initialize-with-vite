7c96312e7d03d24ed4be4c9cf191f2b393ef2edb
Curtisdak <curtis.dakouri@gmail.com>
2024-03-28 16:57:45 +0100

    state.1 is DONE . now less go to the next one

--- src/App.jsx
+++ src/App.jsx
@@ -2,38 +2,46 @@ import PokemonCard from './components/PokemonCard'
 import PropTypes from "prop-types"
 
 import './App.css'
+import { useState } from 'react'
 
 
 function App() {
-
   const pokemonList = [
     {
-      name: "Ballurus",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-      imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/19.png",
-        
-    },
-
-    {
-        name: "kirikou",
-        imgSrc:"",
-       
-    },
-  ]
+        name: "bulbasaur",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      },
+      {
+        name: "charmander",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+      },
+      {
+        name: "squirtle",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+      },
+      {
+        name: "pikachu",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+      },
+      {
+        name: "mew",
+      },
+    ];
+  const [index, setIndex] = useState(0);
 
   // const pokemon = { name:"mew"};
   return (
     <>
     <div className='PokemonCard-container'>
-      <PokemonCard pokemon={pokemonList[0]}/>
-      <PokemonCard pokemon={pokemonList[1]}/>
-      <PokemonCard pokemon={pokemonList[0]}/>
+      <PokemonCard pokemon={pokemonList[index]}/>
     </div> 
+
+    {index > 0 && <button onClick={() => setIndex(index - 1)}>Prev</button> }
+    {index < pokemonList.length - 1 ? <button onClick={() => setIndex(index + 1)}> more </button> : <button className='disabled'> more</button>}
     </>
   )
   
